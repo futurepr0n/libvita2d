5c571af4844fdd4d2fe9c066b5ddade66234766d
Mark Pereira <mark@futurepr0n.com>
2017-02-02 19:52:05 -0500

    added a new sprite (attempted to I should say)

--- sample/main.c
+++ sample/main.c
@@ -15,6 +15,7 @@
  * Symbol of the image.png file
  */
 extern unsigned char _binary_image_png_start;
+extern unsigned char _binary_peter_png_start;
 
 int main()
 {
@@ -34,6 +35,7 @@ int main()
 	 * Load the statically compiled image.png file.
 	 */
 	image = vita2d_load_PNG_buffer(&_binary_image_png_start);
+	peter_image = vita2d_load_PNG_buffer(&_binary_peter_png_start);
 
 	memset(&pad, 0, sizeof(pad));
 
@@ -51,12 +53,15 @@ int main()
 		vita2d_draw_fill_circle(200, 420, 100, RGBA8(0, 255,0 ,255));
 
 		vita2d_draw_texture_rotate(image, 940/2, 544/2, rad);
+		vita2d_draw_texture(peter_image, 20, 20);
+		
+
 
 		vita2d_draw_line(500, 30, 800, 300, RGBA8(255, 0, 255, 255));
 
-		vita2d_pgf_draw_text(pgf, 700, 30, RGBA8(0,255,0,255), 1.0f, "PGF Font sample!");
+		vita2d_pgf_draw_text(pgf, 700, 30, RGBA8(0,255,0,255), 1.0f, "Peter Griffen");
 
-		vita2d_pvf_draw_text(pvf, 700, 80, RGBA8(0,255,0,255), 1.0f, "PVF Font sample!");
+		vita2d_pvf_draw_text(pvf, 700, 80, RGBA8(0,255,0,255), 1.0f, "by futurepr0n!");
 
 		vita2d_end_drawing();
 		vita2d_swap_buffers();
@@ -70,6 +75,7 @@ int main()
 	 */
 	vita2d_fini();
 	vita2d_free_texture(image);
+	vita2d_free_texture(peter_image);
 	vita2d_free_pgf(pgf);
 	vita2d_free_pvf(pvf);
 
